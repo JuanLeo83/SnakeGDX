5975967a44e092295cc9be705de41f794bf8d130
Juan Leo <juanleodev@gmail.com>
2023-02-12 12:24:06 +0100

    Project created

---
+++ core/src/main/kotlin/com/jgpl/Game.kt
@@ -0,0 +1,6 @@
+package com.jgpl
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Game : ApplicationAdapter()
